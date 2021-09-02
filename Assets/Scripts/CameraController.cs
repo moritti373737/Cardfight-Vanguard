85df25ab3e648d6da1a228c7baa7a8864d2ecdcb
moritti <moritti373737@gmail.com>
2021-09-02 16:55:23 +0900

    カーソルをカメラに追従させる

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector3 diff;
+    GameObject target;
+
+    void Start()
+    {
+        target = SelectManager.Instance.SelectBox;
+        diff = target.transform.position - transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.transform.position - diff, Time.deltaTime * 3.0f);
+    }
+}
